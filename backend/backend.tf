86f1d9dc1ca493b52d807208ffa7058922eed352
Samudini Chamodya <159619462+Samudini-Chamodya@users.noreply.github.com>
2025-09-07 17:04:57 +0530

    Update backend.tf

---
+++ backend/backend.tf
@@ -0,0 +1,21 @@
+# setup-backend.tf
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "s3statebackend2"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
